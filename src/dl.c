fd5c3372ddc209507c811ff10a4c99b9ad5ebca7
Mark van der Loo <mark.vanderloo@gmail.com>
2013-04-02 00:00:00 +0000

    version 0.4-0

---
+++ src/dl.c
@@ -0,0 +1,206 @@
+/* This code is gratefully based on Nick Logan's github repository
+ * https://github.com/ugexe/Text--Levenshtein--Damerau--XS/blob/master/damerau-int.c
+ * 
+ *
+ * Changes/additions wrt original code:
+ * - Added R.h, Rdefines.h inclusion
+ * - Added R interface function
+ * - Added edit weights (function is now of type double)
+ * - Added corner cases for length-zero strings.
+ * - Replaced linked list dictionary with fixed-size struct for loop externalization.
+ * - Externalized allocation of dynamic programming matrix.
+ * 
+ * mark.vanderloo@gmail.com
+ */
+
+/* ugexe@cpan.org (Nick Logan)    */
+
+#include <R.h>
+#include <Rdefines.h>
+#include "utils.h"
+
+
+/* Our unsorted dictionary  */
+/* Note we use character ints, not chars. */
+
+
+typedef struct {
+  unsigned int *key;
+  unsigned int *value;
+  unsigned int length;
+} dictionary;
+
+static void reset_dictionary(dictionary *d){
+  int nbytes = sizeof(unsigned int)*d->length;
+  memset(d->key  , 0, nbytes);
+  memset(d->value, 0, nbytes);
+}
+
+static dictionary *new_dictionary(unsigned int length){
+  dictionary *d = (dictionary *) malloc(sizeof(dictionary));
+  if ( d == NULL ){
+    return NULL;
+  }
+  d->key   = (unsigned int *) malloc(length*sizeof(int));
+  d->value = (unsigned int *) malloc(length*sizeof(int));
+  if ( d->key == NULL || d->value == NULL){
+    free(d->key);
+    free(d->value);
+    free(d);
+    return NULL;
+  }
+  d->length = length;
+  reset_dictionary(d);
+  return d;
+}
+
+static void free_dictionary(dictionary *d){
+  free(d->key);
+  free(d->value);
+  free(d);
+}
+
+static void uniquePush(dictionary *d, unsigned int key){
+  int i=0;
+  while (d->key[i] && d->key[i] != key){
+    ++i;
+  }
+  d->key[i] = key;
+}
+
+static unsigned int which(dictionary *d, unsigned int key){
+  int i=0;
+  while( d->key[i] != key ){
+     ++i;
+  }
+  return i;
+}
+
+/* End of Dictionary Stuff */
+
+
+/* All calculations/work are done here */
+
+static double distance(
+      unsigned int *src,
+      unsigned int *tgt,
+      unsigned int x,
+      unsigned int y,
+      double *weight,
+      double maxDistance,
+      dictionary *dict,
+      double *scores
+    ){
+  if ( x==0 ) return y;
+  if ( y==0 ) return x;
+
+  unsigned int swapCount, targetCharCount,i,j;
+  double delScore, insScore, subScore, swapScore;
+  unsigned int score_ceil = x + y;
+
+  /* intialize matrix start values */
+  scores[0] = score_ceil;  
+  scores[1 * (y + 2) + 0] = score_ceil;
+  scores[0 * (y + 2) + 1] = score_ceil;
+  scores[1 * (y + 2) + 1] = 0;
+
+  uniquePush(dict,src[0]);
+  uniquePush(dict,tgt[0]);
+  /* work loops    */
+  /* i = src index */
+  /* j = tgt index */
+  for(i=1;i<=x;i++){ 
+    uniquePush(dict,src[i]);
+    scores[(i+1) * (y + 2) + 1] = i;
+    scores[(i+1) * (y + 2) + 0] = score_ceil;
+
+    swapCount = 0;
+    for(j=1;j<=y;j++){
+      if(i == 1) {
+        uniquePush(dict,tgt[j]);
+        scores[1 * (y + 2) + (j + 1)] = j;
+        scores[0 * (y + 2) + (j + 1)] = score_ceil;
+      }
+      targetCharCount = dict->value[which(dict, tgt[j-1])];
+      swapScore = scores[targetCharCount * (y + 2) + swapCount] + i - targetCharCount - 2 + j - swapCount + weight[3];
+
+      if(src[i-1] != tgt[j-1]){
+        subScore = scores[i * (y + 2) + j] + weight[2];
+        insScore = scores[(i+1) * (y + 2) + j] + weight[1];
+        delScore = scores[i * (y + 2) + (j + 1)] + weight[0];
+        scores[(i+1) * (y + 2) + (j + 1)] = min2(swapScore, min3(delScore, insScore, subScore));
+      } else {
+        swapCount = j;
+        scores[(i+1) * (y + 2) + (j + 1)] = min2(scores[i * (y + 2) + j], swapScore);
+      } 
+    }
+
+    /* We will return -1 here if the */
+    /* current score > maxDistance   */
+    if(maxDistance != 0 && maxDistance < scores[(i+1) * (y + 2) + (y+1)]) {
+      reset_dictionary(dict);
+      return -1;
+    }
+
+      dict->value[which(dict,src[i-1])] = i;    
+  }
+
+  {
+  double score = scores[(x+1) * (y + 2) + (y + 1)];
+  reset_dictionary(dict);
+  return score;
+  }
+}
+
+/* End of workhorse */
+
+// -- interface with R 
+
+
+SEXP R_dl(SEXP a, SEXP b, SEXP weight, SEXP maxDistance){
+  PROTECT(a);
+  PROTECT(b);
+  PROTECT(maxDistance);
+  PROTECT(weight);
+   
+  int i, j, k;
+  int na = length(a);
+  int nb = length(b);
+  int nt = (na > nb) ? na : nb;
+  double maxDist = REAL(maxDistance)[0];
+  double *w = REAL(weight);
+
+  SEXP yy; 
+  PROTECT(yy = allocVector(REALSXP, nt));
+  double *y = REAL(yy);
+
+  /* claim space for workhorse */
+  int max_a = max_length(a);
+  int max_b = max_length(b);
+  dictionary *dict = new_dictionary( max_length(a) + max_length(b) + 1 );
+  double *scores = (double *) malloc( (max_a + 3) * (max_b + 2) * sizeof(double) );
+
+  for ( k=0; k < nt; ++k ){
+    i = k % na;
+    j = k % nb;
+    if ( INTEGER(VECTOR_ELT(a,i))[0] == NA_INTEGER || INTEGER(VECTOR_ELT(b,j))[0] == NA_INTEGER){
+      y[k] = NA_REAL;
+      continue;
+    }
+    y[k] = distance(
+      INTEGER(VECTOR_ELT(a,i)),
+      INTEGER(VECTOR_ELT(b,j)),
+      length(VECTOR_ELT(a,i)),
+      length(VECTOR_ELT(b,j)),
+      w,
+      maxDist,
+      dict,
+      scores
+    );
+  }
+  free_dictionary(dict);
+  free(scores);
+  UNPROTECT(5);
+  return yy;
+} 
+
---
+++ src/hamming.c
@@ -0,0 +1,64 @@
+/* Hamming distance function
+ */
+
+#include <R.h>
+#include <Rdefines.h>
+
+int hamming(unsigned int *a, unsigned int *b, int n, int maxDistance){
+   int i, h=0;
+   for(i=0; i<n; ++i){
+      h += (a[i] == b[i]) ? 0 : 1;
+      if ( maxDistance > 0 && maxDistance < h ){
+         return -1;
+      }
+   }
+   return h;
+}
+
+
+// -- R interface
+
+SEXP R_hm(SEXP a, SEXP b, SEXP maxDistance){
+   PROTECT(a);
+   PROTECT(b);
+   PROTECT(maxDistance);
+
+   int na = length(a);
+   int nb = length(b);
+   int nt = ( na > nb) ? na : nb;
+   SEXP yy;
+   PROTECT(yy = allocVector(INTSXP,nt));
+   int *y = INTEGER(yy);
+   int i,j,k,nchar;
+   int maxDist = INTEGER(maxDistance)[0];
+
+   for ( k=0; k<nt; ++k){
+      i = k % na;
+      j = k % nb;
+      if ( INTEGER(VECTOR_ELT(a,i))[0] == NA_INTEGER || INTEGER(VECTOR_ELT(b,j))[0] == NA_INTEGER ){
+         y[k] = NA_INTEGER;
+         continue;         
+      }
+      nchar = length(VECTOR_ELT(a,i));
+      if ( nchar != length(VECTOR_ELT(b,j)) ){
+         error("Characters strings a[%d] and b[%d] have different number of characters", i, j);
+      }
+      y[k] = hamming(
+         INTEGER(VECTOR_ELT(a,i)),
+         INTEGER(VECTOR_ELT(b,j)),
+         nchar,
+         maxDist
+      );
+   }
+   UNPROTECT(4);
+   return yy;
+
+}
+/*
+#include <stdio.h>
+void main(){
+   int h = hamming("aa","ab",0);
+   printf("h = %d\n",h);
+}
+
+*/
