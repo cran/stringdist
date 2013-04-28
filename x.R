a9c53a218dc6d7afedc1a5a5b51cb5ff45417786
Mark van der Loo <mark.vanderloo@gmail.com>
2013-04-28 00:00:00 +0000

    version 0.6.1

--- x.R
+++
@@ -1,9 +0,0 @@
-
-
-dyn.load('src/dl.so')
-.Call('R_dl',"aa","bb",as.integer(2),as.integer(2),as.integer(3))
-
-
-stringdist('aap',c('noot','mies'),method='dl')
-stringdist(c('noot','mies'),'aap',method='dl')
-
