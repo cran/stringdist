fb800a7b606cf532d6fe5a4c7b21e8952d0098ee
Mark van der Loo <mark.vanderloo@gmail.com>
2018-06-08 12:52:58 +0000

    version 0.9.5.1

--- inst/include/stringdist_api.h
+++ inst/include/stringdist_api.h
@@ -21,9 +21,6 @@
 #ifndef _STRINGDIST_API_H
 #define _STRINGDIST_API_H
 
-#include <stringdist.h>		// also includes R.h, Rinternals.h, Rdefines.h
-
-#include <Rconfig.h>
 #include <R_ext/Rdynload.h>
 
 #ifdef HAVE_VISIBILITY_ATTRIBUTE
@@ -52,9 +49,10 @@ extern "C" {
  * #include <stringdist_api.h>
  * ```
  * 
- * An example of a package using this API is 
- * [refinr](https://CRAN.R-project.org/package=refinr).
- *
+ * 
+ * An example of a published package using this API is
+ * [refinr](https://CRAN.R-project.org/package=refinr). A minimal example can be
+ * found [here](https://github.com/markvanderloo/linkstringdist).
  *
  * @section encoding Character encoding
  * All `character` vector input is expected to be in `UTF-8` (this also allows
@@ -66,12 +64,10 @@ extern "C" {
  *
  * @section threads Thread safety
  *
- * It is not safe to call the stringdist API from multiple concurrent threads.
- * 
- * @section Parallelization
- * 
- * The functions exposed by this API use `openMP` to parallelize computations
- * over the number of threads assigned by the user.
+ * It is not safe to call functions from `stringdist` C API from 
+ * multiple concurrent threads.
+ *
+ *
  *
  */
 
@@ -83,6 +79,14 @@ extern "C" {
  */
 
 
+/*
+SEXP attribute_hidden sd_all_int(SEXP X)
+{
+  static SEXP(*fun)(SEXP) = NULL;
+  if (fun == NULL) fun = (SEXP(*)(SEXP)) R_GetCCallable("stringdist","R_all_int");
+  return fun(X);
+}
+*/
 
 /**
  * @brief Find the location of values in `x` in `table` by approximate matching.
@@ -103,9 +107,9 @@ extern "C" {
  *    - 9: Soundex (`"soundex"`)
  *   @endparblock
  * @param nomatch `[integer]` The value to be returned when no match is found. 
- * @param matchNA Should `NA`s be matched? Default behaviour mimics the 
- *  behaviour of base `match`, meaning that `NA` matches
- *  `NA` (see also the note on `NA` handling below).
+ * @param matchNA Should `NA`s be matched? Default behaviour mimics the
+ *   behaviour of base `match`, meaning that `NA` matches `NA` (see also the note
+ *   on `NA` handling below).
  * @param weight `[numeric]` vector. Edit penalty
  *   @parblock
  *     For `method='osa'` or`'dl'`, the penalty for
@@ -116,16 +120,17 @@ extern "C" {
  *     Weights must be positive and not exceed 1. `weight` is ignored
  *     completely for other methods
  *   @endparblock
- * @param q  `[integer]` scalar. Size of the q-gram; must be nonnegative. Only applies to
- *   `method='qgram'`, `'jaccard'` or `'cosine'`.
+ * @param q  `[integer]` scalar. Size of the q-gram; must be nonnegative. Only
+ *   applies to `method='qgram'`, `'jaccard'` or `'cosine'`.
  * @param maxDistance `[numeric]` scalar. The maximum distance allowed for matching.
- * @param p `[numeric]` scalar. Penalty factor for Jaro-Winkler distance. The valid range for 
- *   `p` is `0 <= p <= 0.25`. If `p=0` (default), the
+ * @param p `[numeric]` scalar. Penalty factor for Jaro-Winkler distance. The
+ *   valid range for `p` is `0 <= p <= 0.25`. If `p=0` (default), the
  *   Jaro-distance is returned. Applies only to `method='jw'`.
- * @param bt `[numeric]` vector. Winkler's boost threshold. Winkler's penalty factor is
- *   only applied when the Jaro distance is larger than `bt`.
- *   Applies only to `method='jw'` and `p>0`.
- * @param useBytes Perform byte-wise comparison (i.e. do not translate UTF-8 to integer prior to distance calculation)
+ * @param bt `[numeric]` vector. Winkler's boost threshold. Winkler's penalty
+ *   factor is only applied when the Jaro distance is larger than `bt`.  Applies
+ *   only to `method='jw'` and `p>0`.
+ * @param useBytes Perform byte-wise comparison (i.e. do not translate UTF-8 to
+ *   integer prior to distance calculation)
  * @param nthread `[integer]` scalar. Maximum number of threads to use. 
  *
  *
@@ -150,9 +155,11 @@ SEXP attribute_hidden sd_amatch(SEXP x, SEXP table, SEXP method
  * @param qq `[integer`] scalar. 
  * 
  * @return
- * A `[numeric]` vector of `length(a)*n_qgrams`, where `n_qrams` is the number of different `qgrams` observed
- * in the elements of `a`. The output vector has an attribute called `qgrams`, which is an integer vector
- * of size `q*n_qgrams` containing integer (UTF-32) labels for the q-grams sequentially.
+ * A `[numeric]` vector of `length(a)*n_qgrams`, where `n_qrams` is the number
+ * of different `qgrams` observed in the elements of `a`. The output vector has
+ * an attribute called `qgrams`, which is an integer vector of size
+ * `q*n_qgrams` containing integer (UTF-32) labels for the q-grams
+ * sequentially.
  * 
  */
 SEXP attribute_hidden sd_get_qgrams(SEXP a, SEXP qq)
@@ -162,6 +169,14 @@ SEXP attribute_hidden sd_get_qgrams(SEXP a, SEXP qq)
   return fun(a, qq);
 }
 
+/*
+SEXP attribute_hidden sd_lengths(SEXP X)
+{
+  static SEXP(*fun)(SEXP) = NULL;
+  if (fun == NULL) fun = (SEXP(*)(SEXP)) R_GetCCallable("stringdist","R_lengths");
+  return fun(X);
+}
+*/
 
 /**
  * @brief Lower tridiagonal elements of distance matrix.
@@ -182,28 +197,29 @@ SEXP attribute_hidden sd_get_qgrams(SEXP a, SEXP qq)
  *   @endparblock
  * @param weight `[numeric]` vector. Edit penalty
  *   @parblock
- *     For `method='osa'` or`'dl'`, the penalty for
- *     deletion, insertion, substitution and transposition, in that order. When
- *     `method='lv'`, the penalty for transposition is ignored. When
- *     `method='jw'`, the weights associated with characters of `a`,
- *     characters from `b` and the transposition weight, in that order. 
- *     Weights must be positive and not exceed 1. `weight` is ignored
- *     completely for other methods
+ *     For `method='osa'` or`'dl'`, the penalty for deletion, insertion,
+ *     substitution and transposition, in that order. When `method='lv'`, the
+ *     penalty for transposition is ignored. When `method='jw'`, the weights
+ *     associated with characters of `a`, characters from `b` and the
+ *     transposition weight, in that order.  Weights must be positive and not
+ *     exceed 1. `weight` is ignored completely for other methods
  *   @endparblock
- * @param q  `[integer]` scalar. Size of the q-gram; must be nonnegative. Only applies to
- *   `method='qgram'`, `'jaccard'` or `'cosine'`.
- * @param p `[numeric]` scalar. Penalty factor for Jaro-Winkler distance. The valid range for 
- *   `p` is `0 <= p <= 0.25`. If `p=0` (default), the
+ * @param q  `[integer]` scalar. Size of the q-gram; must be nonnegative. Only
+ *   applies to `method='qgram'`, `'jaccard'` or `'cosine'`.
+ * @param p `[numeric]` scalar. Penalty factor for Jaro-Winkler distance. The
+ *   valid range for `p` is `0 <= p <= 0.25`. If `p=0` (default), the
  *   Jaro-distance is returned. Applies only to `method='jw'`.
- * @param bt `[numeric]` vector. Winkler's boost threshold. Winkler's penalty factor is
- *   only applied when the Jaro distance is larger than `bt`.
- *   Applies only to `method='jw'` and `p>0`.
- * @param useBytes Perform byte-wise comparison (i.e. do not translate UTF-8 to integer prior to distance calculation)
+ * @param bt `[numeric]` vector. Winkler's boost threshold. Winkler's penalty
+ *   factor is only applied when the Jaro distance is larger than `bt`.  Applies
+ *   only to `method='jw'` and `p>0`.
+ * @param useBytes Perform byte-wise comparison (i.e. do not translate UTF-8 to
+ *   integer prior to distance calculation)
  * @param nthread `[integer]` scalar. Maximum number of threads to use. 
  *
  * @return
- * A `[numeric]` vector of length `n*(n-1)/2`, where `n=length(a)`. It contains the positive values of consequtive columns
- * of the distance matrix. Also see the R-code in  `stringdist:::lower_tri`.
+ *   A `[numeric]` vector of length `n*(n-1)/2`, where `n=length(a)`. It contains
+ *   the positive values of consequtive columns of the distance matrix. Also see
+ *   the R-code in  `stringdist:::lower_tri`.
  */
 SEXP attribute_hidden sd_lower_tri(SEXP a, SEXP method
                                      , SEXP weight, SEXP p,  SEXP bt, SEXP q
@@ -222,7 +238,9 @@ SEXP attribute_hidden sd_lower_tri(SEXP a, SEXP method
  *
  * @return
  *
- * A character vector of `length(x)` with soundex codes for elements of `x`.
+ * A `list` with `length(x)` element. Each element is a length 4 integer vector 
+ * representing a 4-character soundex code. The integers are ASCII code points.
+ *
  */
 SEXP attribute_hidden sd_soundex(SEXP x, SEXP useBytes)
 {
@@ -251,29 +269,30 @@ SEXP attribute_hidden sd_soundex(SEXP x, SEXP useBytes)
  *   @endparblock
  * @param weight `[numeric]` vector. Edit penalty
  *   @parblock
- *     For `method='osa'` or`'dl'`, the penalty for
- *     deletion, insertion, substitution and transposition, in that order. When
- *     `method='lv'`, the penalty for transposition is ignored. When
- *     `method='jw'`, the weights associated with characters of `a`,
- *     characters from `b` and the transposition weight, in that order. 
- *     Weights must be positive and not exceed 1. `weight` is ignored
- *     completely for other methods
+ *     For `method='osa'` or`'dl'`, the penalty for deletion, insertion,
+ *     substitution and transposition, in that order. When `method='lv'`, the
+ *     penalty for transposition is ignored. When `method='jw'`, the weights
+ *     associated with characters of `a`, characters from `b` and the
+ *     transposition weight, in that order.  Weights must be positive and not
+ *     exceed 1. `weight` is ignored completely for other methods
  *   @endparblock
- * @param q  `[integer]` scalar. Size of the q-gram; must be nonnegative. Only applies to
- *   `method='qgram'`, `'jaccard'` or `'cosine'`.
- * @param p `[numeric]` scalar. Penalty factor for Jaro-Winkler distance. The valid range for 
- *   `p` is `0 <= p <= 0.25`. If `p=0` (default), the
+ * @param q  `[integer]` scalar. Size of the q-gram; must be nonnegative. Only
+ *   applies to `method='qgram'`, `'jaccard'` or `'cosine'`.
+ * @param p `[numeric]` scalar. Penalty factor for Jaro-Winkler distance. The
+ *   valid range for `p` is `0 <= p <= 0.25`. If `p=0` (default), the
  *   Jaro-distance is returned. Applies only to `method='jw'`.
- * @param bt `[numeric]` vector. Winkler's boost threshold. Winkler's penalty factor is
- *   only applied when the Jaro distance is larger than `bt`.
- *   Applies only to `method='jw'` and `p>0`.
- * @param useBytes Perform byte-wise comparison (i.e. do not translate UTF-8 to integer prior to distance calculation)
+ * @param bt `[numeric]` vector. Winkler's boost threshold. Winkler's penalty
+ *   factor is only applied when the Jaro distance is larger than `bt`.  Applies
+ *   only to `method='jw'` and `p>0`.
+ * @param useBytes Perform byte-wise comparison (i.e. do not translate UTF-8 to
+ *   integer prior to distance calculation)
  * @param nthread `[integer]` scalar. Maximum number of threads to use. 
  *
  *
  * @return
- * A `[numeric]` vector of length `max(length(a),length(b))` where the shortest vector is recycled over the
- * longer (no warnings are given when the longer length is not an integer multiple of the shorter length).
+ *   A `[numeric]` vector of length `max(length(a),length(b))` where the shortest
+ *   vector is recycled over the longer (no warnings are given when the longer
+ *   length is not an integer multiple of the shorter length).
  *
  *
  */
